0c85d84ebb2582e19afb9262d3354212f93f35dd
qiqetes <qiqete@gmail.com>
2020-06-06 10:06:50 +0200

    Added trigram functionality to improve the bot

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tweet creator with ML
-This is a script that uses markov-chains to parse tweets written by any user and create tweets using what he says.
+This is a script that uses markov-chains to parse tweets written by any user and create tweets using what they say.
 
 ## Usage
 You need to create a .json file called secret.json storing there as variables the API_key, API_secret_key, Access_token and Access_token_secret that you can obtain from the [Twitter API](https://developer.twitter.com/en/docs)
--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import argparse
 from nltk.tokenize import word_tokenize
 from collections import Counter
 import random
+from pathlib import Path
 
 
 class Main:
 
     def __init__(self):
-        with open('secret.json') as data_file:
-            data = json.load(data_file)
+
+        if Path('secret.json').exists():
+            with open('secret.json') as data_file:
+                data = json.load(data_file)
+        else:
+            raise Exception("Neeed to create a secret.json file with your API keys")
         consumer_key = data['API_key']  # Add your API key here
         consumer_secret = data['API_secret_key']  # Add your API secret key here
         access_token = data['Access_token']
@@ -22,16 +27,44 @@ class Main:
         self.auth.set_access_token(access_token, secret_access_token)
 
     def main(self):
-        # parser = argparse.ArgumentParser(description='Analize tweets of a user to make a markov chain of it')
-        # parser.add_argument('-R', '--retrieve', dest='retr', action='store_true', default=False, help='retrieve some of the user tweets.')
-        # parser.add_argument('-M', '--markov', dest='mark', action='store_true', default=False, help='do the markov chain with the data stored of that user.')
+        parser = argparse.ArgumentParser(description='Analize tweets of a user to make a markov chain of it')
+        users_group = parser.add_mutually_exclusive_group(required=True)
+        users_group.add_argument('-u', '--username', type=str, help="username you want to work with")
+        users_group.add_argument('-U', '--usernames', type=str, help="usernames separated by commas")
+        parser.add_argument('-R', '--retrieve', dest='retr', action='store_true', default=False, help='retrieve some of the user tweets and store them in a json file')
+        parser.add_argument('-M', '--markov', dest='mark', action='store_true', default=False, help='do the markov chain with the data stored of that user.')
 
-        username = "realDonaldTrump"
+        args = parser.parse_args()
 
-        # self.retrieve_tweets(username)
+        usernames = []
+        if args.username:
+            usernames.append(args.username)
+            print("The username: " + usernames[0])
+        elif args.usernames:
+            usernames_str = args.usernames
+            usernames = usernames_str.split(",")
+            for user in usernames:
+                user = user.strip()
+                print("The username: " + user + " was added")
 
-        # TOKENIZATION
-        self.tokenize_tweets(username)
+        if args.mark:
+            # TODO: rename the results.json to tokens.json or tokens.bin
+            if Path('result.json').exists():
+                # FIXME: only working for a single user
+                self.tokenize_tweets(usernames[0])
+            else:
+                raise Exception("There is no retrieved data for that user, you may run this command with the argument --retrieve")
+
+        elif args.retr:
+            # FIXME: only working for a single user
+            self.retrieve_tweets(usernames[0])
+
+        else:
+            # Do the entire process
+            for user in usernames:
+                self.retrieve_tweets(user)
+
+            self.tokenize_tweets(usernames[0])
 
     def tokenize_tweets(self, username):
         # Do we have a list of tweets of that user?
@@ -76,14 +109,26 @@ class Main:
 
         bigrams = {}  # one word and the next one with the counter
         trigrams = {}
+        # FIXME: the "." handling is off
         for tweet_tokenized in tweets_tokenized:
             for i, token in enumerate(tweet_tokenized):
+                # BIGRAMS
                 if token not in bigrams:
                     bigrams[token] = []
                 if i+1 < len(tweet_tokenized):
                     bigrams[token].append(tweet_tokenized[i+1])
                 else:
-                    bigrams[token].append(".")
+                    bigrams[token].append(".")  # FIXME: the "." handling is off
+
+                # TRIGRAMS
+                if i+1 < len(tweet_tokenized):
+                    token = str(token) + " " + tweet_tokenized[i+1]
+                    if token not in trigrams:
+                        trigrams[token] = []
+                    if i+2 < len(tweet_tokenized):
+                        trigrams[token].append(tweet_tokenized[i+2])
+                    else:
+                        trigrams[token].append(".")  # FIXME: the "." handling is off
 
         for token in bigrams:
             aux = Counter(bigrams[token])
@@ -92,43 +137,70 @@ class Main:
                 aux[k] = aux[k]/num
             bigrams[token] = aux
 
-        # with open('bigrams.json', 'w') as fp:
-        #     json.dump(bigrams, fp)
-        self.create_random_tweet(bigrams)
+        for token in trigrams:
+            aux = Counter(trigrams[token])
+            num = len(trigrams[token])
+            for k in aux:
+                aux[k] = aux[k]/num
+            trigrams[token] = aux
 
-    def create_random_tweet(self, bigrams):
-        # First word is 0
-        # Last word is .
-        tweet_tokens = [0]
-        i = 0
+        with open('trigrams.json', 'w') as fp:
+            json.dump(trigrams, fp)
+
+        self.create_random_tweet(bigrams, trigrams=trigrams)
+
+    def create_random_tweet(self, bigrams, trigrams={}):
+        max_len_tweet = 140
+
         # 'cute': Counter({'pero': 0.25, 'please': 0.25, 'to': 0.25, '.': 0.25})
-        while tweet_tokens[-1] != '.' and len(" ".join(tweet_tokens[1:])) < 140:
-            token = tweet_tokens[-1]
-            prob = 0
-            rand = random.uniform(0, 1)
-            for word in bigrams[token]:
-                if(bigrams[token][word] + prob > rand):
+        tweet_tokens = [0]
+
+        # First word selected on bigrams
+        token = "0"
+        while token == "0":
+            for word in bigrams[0]:
+                prob = 0
+                rand = random.uniform(0, 1)
+                if(bigrams[0][word] + prob > rand):
                     tweet_tokens.append(word)
                     break
                 else:
-                    prob += bigrams[token][word]
+                    prob += bigrams[0][word]
 
-        print(" ".join(tweet_tokens[1:-1])+".")
+            tweet_tokens[0] = "0"
+            token = " ".join(tweet_tokens[-2:])
+        while tweet_tokens[-1] != '.' and len(" ".join(tweet_tokens[1:])) < max_len_tweet:
+            token = " ".join(tweet_tokens[-2:])
+
+            for word in trigrams[token]:
+                if(trigrams[token][word] + prob > rand):
+                    tweet_tokens.append(word)
+                    break
+                else:
+                    prob += trigrams[token][word]
+
+        print("=======================================\n Generated tweet: \n")
+        print(" ".join(tweet_tokens[1: -1])+".")
 
     def retrieve_tweets(self, username):
         print("Retrieving", username, "timeline")
 
         # Get the acutal list of retrieved tweets
         tweets = []
-        with open('result.json') as f:
-            tweets = json.load(f)
+
+        if Path('result.json').exists():
+            with open('result.json') as f:
+                tweets = json.load(f)
 
         # Getting the API
         api = tweepy.API(self.auth)
 
         count = 300
         tweets_count = 0
-        while count > 1 and tweets_count < 4800:
+        non_rt_count = 0
+        last_tweet_count = 0
+        repeats = 0
+        while count > 1 and tweets_count < 3200 and repeats < 3:
             # Getting the last tweet of that username
             max_id = math.inf
             found = False
@@ -148,28 +220,24 @@ class Main:
                     t = {"id": tweet.id, "user": username, "text": tweet.full_text, "date": str(tweet.created_at)}
                     if t not in tweets:
                         tweets.append(t)
-                        print("Tweet #" + str(tweets_count) + ": ")
-                        print(tweet.full_text[0:40]+"...")
+                        # print("Tweet #" + str(tweets_count) + ": ")
+                        # print(tweet.full_text[0:40]+"...")
                         tweets_count += 1
+                        non_rt_count += 1
 
             with open('result.json', 'w') as fp:
                 json.dump(tweets, fp)
 
             count -= 1
             print("Time left to retrieve: " + str(count * 3) + "s")
-            print("Total tweets analized: " + str(tweets_count))
-            time.sleep(3)
+            print("Total tweets analized: " + str(tweets_count) + "\t\t non RT: "+str(non_rt_count))
 
+            if last_tweet_count == non_rt_count:
+                repeats += 1
 
-# class Tweet:
-#     def __init__(self, id, text, date):
-#         self.id = id
-#         self.text = text
-#         self.date = date
+            last_tweet_count = non_rt_count
 
-#     def toJSON(self):
-#         return json.dumps(self, default=lambda o: o.__dict__,
-#                           sort_keys=True, indent=4)
+            time.sleep(1.1)  # You can lower it to 1 but it's too adjusted to the API limits
 
 
 if __name__ == "__main__":
